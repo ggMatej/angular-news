48cba400abf2f90b620433a0d33dc005d5871c5a
Matej Mijić <matej@protoyp.digital>
2020-05-15 13:58:11 +0200

    Added ngrx-local-storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { ArticlesModule } from './modules/articles/articles.module';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -13,6 +12,26 @@ import { articlesReducer } from './modules/articles/store/articles.reducer';
 import { ArticlesEffects } from './modules/articles/store/articles.effect';
 import { ArticlesComponent } from './modules/articles/views/articles/articles.component';
 import { ArticleDetailsComponent } from './modules/articles/views/article-details/article-details.component';
+
+import {
+  StoreModule,
+  ActionReducer,
+  MetaReducer,
+  ActionReducerMap,
+} from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
+import { reducers } from './modules/ngrx-store/Reducers';
+import { ApplicationState } from './modules/ngrx-store/ApplicationState';
+
+export function localStorageSyncReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return localStorageSync({ keys: ['articles'], rehydrate: true })(reducer);
+}
+
+const persistedReducers: ActionReducerMap<ApplicationState> = reducers;
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -24,9 +43,7 @@ import { ArticleDetailsComponent } from './modules/articles/views/article-detail
       { path: '', component: ArticlesComponent },
       { path: 'articles/:articleId', component: ArticleDetailsComponent },
     ]),
-    StoreModule.forRoot({
-      articles: articlesReducer,
-    }),
+    StoreModule.forRoot(persistedReducers, { metaReducers }),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
     }),
